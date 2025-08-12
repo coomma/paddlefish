d25ca1f0a938aceb32cfffc1d734d4f428bffd03
jun qi <qijun.net@gmail.com>
2025-08-12 02:18:13 +0000

    Error occurred prerendering page "/en/stories/the-last-sighting". Read m

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,5 +1,5 @@
 import { getRequestConfig } from 'next-intl/server';
-import { locales } from '@/lib/constants';
+import { locales } from './lib/constants';
  
 export default getRequestConfig(async ({ locale }) => {
   // Validate that the incoming `locale` parameter is valid
